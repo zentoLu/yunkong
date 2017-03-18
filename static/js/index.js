9e3cd3d5618d5ecb35cf2a46a7bd64419d1f98c1
zentoLu <491097990@qq.com>
2017-03-18 10:47:55 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,20 @@
 $(function() {
-  $('.flexslider').flexslider({
-    animation: "slide"
-  });
-
+    $('.flexslider').flexslider({
+        animation: "slide"
+    });
+    $("#panel .icons li").not(".up,.down").mouseenter(function() {
+        $("#panel .info").addClass('hover');
+        $("#panel .info li").hide();
+        $("#panel .info li." + $(this).attr('class')).show();
+    });
+    $("#panel").mouseleave(function() {
+        $("#panel .info").removeClass('hover');
+    })
+    $(".icons .up").click(function() {
+        $.fn.ronbongpage.moveSectionUp();
+    });
+    $(".icons .down").click(function() {
+        $.fn.ronbongpage.moveSectionDown();
+    });
 
 });
